34b0e453375b0f7a9ccb429cf083541db3c73588
chiao <cheng.chiao@positivegrid.com>
2019-07-03 10:56:17 +0800

    Updates

---
+++ precache-manifest.5d57cd7b39947e13fdfb14478d653730.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adb2accd90f06a06a23df8f911517438",
+    "url": "/index.html"
+  },
+  {
+    "revision": "294f1b178dfa34cb2868",
+    "url": "/static/js/2.402793f7.chunk.js"
+  },
+  {
+    "revision": "3e8fd9871393f6d63027",
+    "url": "/static/js/main.4d7b96f3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
